3aca691e594ddac3afffc4f9336930ae486904d7
theo.henri.guilbert-hattermann <theo.henri.guilbert-hattermann@ut.ee>
2025-03-20 11:17:52 +0100

    pm2 pour composant nutrition

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+      {
+        name: "backend",
+        script: "backend/serveur.js",
+        watch: true,  // Redémarre si le fichier change
+      },
+    ],
+  };
+  
