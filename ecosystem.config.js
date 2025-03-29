8a7e4d9892d77aca52c42588171df15a3d0e266f
theo.henri.guilbert-hattermann <theo.henri.guilbert-hattermann@ut.ee>
2025-03-29 23:12:06 +0100

    enable pm2 for nutrition api (autostart)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "nutrition_api.js",
-        script: "backend/nutrition_api.js",
+        script: "backend/nutrition/nutrition_api.js",
         watch: true,  // Redémarre si le fichier change
       },
     ],
